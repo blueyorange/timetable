03f2a48e9e74387956d8c819f49c1185ee8463a0
Russell <blueyorange@gmail.com>
2023-07-03 15:55:38 +0100

    moved html into components

--- tt-carousel.js
+++ tt-carousel.js
@@ -1,10 +1,46 @@
 class TimetableCarousel extends HTMLElement {
   constructor() {
     super();
-    const template = document.querySelector("#tt-carousel").content;
-    console.log(template);
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(template.cloneNode(true));
+    this.shadowRoot.innerHTML = `
+      <style>
+        * {
+          box-sizing: border-box;
+        }
+        .slider {
+          width: 100%;
+          text-align: center;
+          overflow: hidden;
+        }
+        slot {
+          display: flex;
+          overflow-x: auto;
+          scroll-snap-type: x mandatory;
+          scroll-behavior: smooth;
+        }
+        ::slotted(*) {
+          scroll-snap-align: start;
+          flex-shrink: 0;
+          width: 100%;
+          height: 500px;
+          transition: transform 0.5s;
+          position: relative;
+          display: flex;
+          justify-content: center;
+        }
+      </style>
+      <div class="slider">
+        <nav>
+          <a href="#slide-1">1</a>
+          <a href="#slide-2">2</a>
+        </nav>
+        <div class="slides"><slot></slot></div>
+      </div>`;
+  }
+  connectedCallback() {
+    const slotElement = this.shadowRoot.querySelector("slot");
+    const slottedElements = slotElement.assignedElements({ flatten: true });
+    console.log(slottedElements);
   }
 }
 
--- tt-week.js
+++ tt-week.js
@@ -1,9 +1,52 @@
+function generateClockTimes() {
+  const times = [];
+  for (let hour = 8; hour < 9; hour++) {
+    for (let minute = 0; minute < 60; minute++) {
+      const formattedHour = hour.toString().padStart(2, "0");
+      const formattedMinute = minute.toString().padStart(2, "0");
+      const time = `${formattedHour}${formattedMinute}`;
+      times.push(time);
+    }
+  }
+
+  return times;
+}
+
 class TimetableWeek extends HTMLElement {
   constructor() {
     super();
-    const template = document.querySelector("#tt-week").content;
     this.attachShadow({ mode: "open" });
-    this.shadowRoot.appendChild(template.cloneNode(true));
+    this.shadowRoot.innerHTML = `<style>
+        .container {
+          display: grid;
+          width: 100%;
+          grid-template-columns: 1fr 1fr 1fr 1fr 1fr;
+          grid-template-rows: [col-head] 1em ${generateClockTimes()
+            .map((time) => `[time-${time}] 1fr`)
+            .join("")};
+        }
+        .col-head {
+          grid-row: 1/2;
+        }
+        .row-head {
+          grid-column: 1/2;
+        }
+        ${["monday", "tuesday", "wednesday", "thursday", "friday"]
+          .map((day, index) => {
+            `.${day}, ::slotted(.${day}) {
+            grid-column: ${index + 2} / ${index + 3}
+          }`;
+          })
+          .join("")}
+      </style>
+      <div class="container">
+        <div class="col-head monday">Mon</div>
+        <div class="col-head tuesday">Tues</div>
+        <div class="col-head monday">Weds</div>
+        <div class="col-head">Thu</div>
+        <div class="col-head">Fri</div>
+        <slot></slot>
+      </div>`;
   }
 }
 
